5d4c6f0df4d2b0bcfe62f511375a5e13395a306e
msineath <msineath@LAPTOP-65OGSQM1.localdomain>
2021-03-01 18:55:50 -0500

    working on bonus leetcode

--- problems/04-bfs.js
+++ problems/04-bfs.js
@@ -1,9 +1,35 @@
+const {TreeNode} = require('./01-tree-node')
+
 // Implement a function that takes in the root of a
 // binary tree, traverses it in depth-first-search
 // order, and returns an array containing the values
 // in the order you visited them.
 function bfs(root){
-    
+    if (!root) return [];
+    let q = [root];
+    let res = [];
+    while (q.length) {
+        let current = q[0];
+        if(current.left) q.push(current.left);
+        if(current.right) q.push(current.right);
+        res.push(q.shift().val);
+    }
+    return res;
 }
+
+let root = new TreeNode('a');
+let b = new TreeNode('b');
+let c = new TreeNode('c');
+let d = new TreeNode('d');
+let e = new TreeNode('e');
+let f = new TreeNode('f');
+
+root.left = b;
+root.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+console.log(bfs(root))
 
 module.exports = { bfs };
--- problems/05-leet-code-105.js
+++ problems/05-leet-code-105.js
@@ -3,6 +3,26 @@
 
 const { TreeNode } = require('./01_tree_node.js');
 
+let preorder = [3,9,20,15,7];
+let inorder = [9, 3, 15, 20, 7];
+
+let preorder2 = [-1];
+let inorder2 = [-1];
+
 function buildTree(preorder, inorder) {
 
 }
+
+// Input: , inorder = [9,3,15,20,7]
+// Output: [3,9,20,null,null,15,7]
+
+// Input: preorder = [-1], inorder = [-1]
+// Output: [-1]
+
+// 1 <= preorder.length <= 3000
+// inorder.length == preorder.length
+// -3000 <= preorder[i], inorder[i] <= 3000
+// preorder and inorder consist of unique values.
+// Each value of inorder also appears in preorder.
+// preorder is guaranteed to be the preorder traversal of the tree.
+// inorder is guaranteed to be the inorder traversal of the tree.
